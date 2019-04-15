984efee12efd7a9fbf3d4c4932101c316de82490
elinwassenaar <elinwassenaar@gmail.com>
2019-04-15 12:15:53 +0200

    shakeEvent toegevoegd

---
+++ script.js
@@ -0,0 +1,57 @@
+// variabelen voor alle DOM-elementen waar ik iets mee wil
+const userInput = document.querySelector('.userinput');
+const addButton = document.querySelector('.addtodo');
+const toDoList = document.querySelector('.todolist');
+
+// add = list item maken, input field weer leeg maken
+addButton.addEventListener('click', () => {
+    if (userInput.value === '') {
+        return;
+    }
+    toDoList.innerHTML += `<li class="listitem"><p>${userInput.value}</p><button class="deletetodo">×</button></li>`;
+    userInput.value = '';
+});
+// hierboven kun je ook 'submit' gebruiken, als je een form in je HTML hebt.
+// dan wel preventDefault omdat ie het niet naar de server hoeft te sturen,
+// maar dan werkt het ook met enter in plaats van alleen met klik
+
+// EN: die += zorgt ervoor dat hele lijst opnieuw geladen wordt met extra element
+// is niet echt best practice ivm performance en bijv. animaties
+
+//variabelen voor afvinken en verwijderen
+const toggleDone = listItem => {
+    listItem.classList.toggle('done');
+};
+
+const deleteItem = deleteToDo => {
+    deleteToDo.remove();
+};
+
+//acties voor afvinken en verwijderen
+toDoList.addEventListener('click', () => {
+    if (event.target.classList.contains('listitem')) {
+        toggleDone(event.target);
+    };
+    if (event.target.classList.contains('deletetodo')) {
+        deleteItem(event.target.closest(".listitem"));
+    };
+});
+
+   //listen to shake event
+    var shakeEvent = new Shake({threshold: 15});
+    shakeEvent.start();
+    window.addEventListener('shake', function(){
+        if (userInput.value === '') {
+            return;
+        }
+        toDoList.innerHTML += `<li class="listitem"><p>${userInput.value}</p><button class="deletetodo">×</button></li>`;
+        userInput.value = '';
+    }, false);
+
+    //stop listening
+    function stopShake(){
+        shakeEvent.stop();
+    }
+
+    //check if shake is supported or not.
+    if(!("ondevicemotion" in window)){alert("Not Supported");}
